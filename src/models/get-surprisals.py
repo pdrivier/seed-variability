9a46450124a7f4bd5e55f29ef4bff6bc48a27ad5
seantrott <trott.sean@gmail.com>
2025-05-23 14:14:12 -0400

    updated data

--- src/models/get-surprisals.py
+++ src/models/get-surprisals.py
@@ -15,9 +15,9 @@ from transformers import GPTNeoXForCausalLM, AutoTokenizer
 ## List all models
 MODELS = [
          # 'EleutherAI/pythia-14m',
-         'EleutherAI/pythia-70m', ### need more steps
-         # 'EleutherAI/pythia-160m', ### need more steps
-         #  'EleutherAI/pythia-410m',
+         # 'EleutherAI/pythia-70m', ### need more steps
+        #  'EleutherAI/pythia-160m', ### need more steps
+          'EleutherAI/pythia-410m',
           # 'EleutherAI/pythia-1b',
           # 'EleutherAI/pythia-1.4b',
           # 'EleutherAI/pythia-2.8b',
